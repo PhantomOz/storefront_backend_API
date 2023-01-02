99396965db11e3669fdf1fb7eaaa1ce2bb5ab75d
PhantomOz <faniogor@gmail.com>
2023-01-02 23:32:08 +0100

    This commit will connect the DB

---
+++ src/utils/database.ts
@@ -0,0 +1,16 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
+  process.env;
+
+const client = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+});
+
+export default client;
