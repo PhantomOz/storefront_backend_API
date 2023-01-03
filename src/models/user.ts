9d587e8a71c53efea7a5bb22c4e996917fee4cd5
PhantomOz <faniogor@gmail.com>
2023-01-03 14:22:56 +0100

    This commit will add Authentication to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,9 @@
 import client from "../utils/database";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 export type User = {
   id: number;
   firstname: string;
@@ -34,15 +38,20 @@ export class UserStore {
   } //Get a particular user with the given id
 
   async create(user: User): Promise<User> {
+    const { BCRYPT_PASSWORD, SALTROUND } = process.env;
     try {
       //ts-ignore
       const conn = await client.connect();
       const sql =
         "INSERT INTO users (firstname, lastname, password) VALUES ($1, $2, $3)";
+      const hash = bcrypt.hashSync(
+        user.password + BCRYPT_PASSWORD,
+        Number(SALTROUND)
+      );
       const result = await conn.query(sql, [
         user.firstname,
         user.lastname,
-        user.password,
+        hash,
       ]);
       const newUser = result.rows[0];
       conn.release();
@@ -50,5 +59,25 @@ export class UserStore {
     } catch (error) {
       throw new Error("Cannot create user in database");
     }
-  }
+  } // Create new Users
+
+  async authenticate(user: User): Promise<string> {
+    const { BCRYPT_PASSWORD, SALTROUND } = process.env;
+
+    const conn = await client.connect();
+    const sql = "SELECT * FROM users WHERE fisrtname=($1) AND lastname=($2)";
+
+    const result = await conn.query(sql, [user.firstname, user.lastname]);
+
+    if (result.rows.length) {
+      const info = result.rows[0];
+
+      if (bcrypt.compareSync(user.password + BCRYPT_PASSWORD, info.password)) {
+        const sign = jwt.sign(info, String(process.env.JWTSECRET));
+        return sign;
+      }
+    }
+
+    return "Invalid credentials";
+  } // Authentication of user credentials
 }
